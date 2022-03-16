ea65291c2926c4d4c5eab58f4ba29216fb402b48
kimagliardi <kim.agliardi@gmail.com>
2022-03-16 19:38:38 -0300

    - Add support to french / spanish - Refactor tests

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,33 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("kim")
-	want := "Hello, kim"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "english")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("", "english")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Shoshana", "French")
+		want := "Bonjour, Shoshana"
+		assertCorrectMessage(t, got, want)
+	})
 }
