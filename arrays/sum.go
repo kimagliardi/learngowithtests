9cb5fbe78e299027d6552ea0e3d6173c14eacb4d
kim.agliardi@gmail.com <kim.agliardi@gmail.com>
2022-07-27 20:13:32 -0300

    Add tests slices section

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,3 +8,27 @@ func Sum(arr []int) int {
 	}
 	return result
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) > 0 {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+
+	return sums
+}
